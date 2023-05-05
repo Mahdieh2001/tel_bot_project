73076579aeb640581c96962d277a0c38e5dae65b
Mahdieh2001 <manouchehrirad2001@gmail.com>
2023-05-05 14:48:45 +0330

    Add three button to help

--- tel_bot.py
+++ tel_bot.py
@@ -5,19 +5,28 @@ bot = telebot.TeleBot("")
 first_button = telebot.types.InlineKeyboardButton("Button 1", url = "https://t.me/wasted_food_0")
 second_button = telebot.types.InlineKeyboardButton("Button 2", url = "https://www.theodinproject.com/dashboard")
 
-markup_hello = telebot.types.InlineKeyboardMarkup()
-markup_help = telebot.types.InlineKeyboardMarkup(row_width=1)
+markup_hello = telebot.types.InlineKeyboardMarkup(row_width=1)
 
 markup_hello.add(first_button, second_button)
-markup_help.add(first_button, second_button)
-
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, "Hi", reply_markup = markup_hello)
 
+key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+key_markup.add("One", "Two", "Three")
+
 @bot.message_handler(commands=['help'])
 def help_me(message):
-    bot.reply_to(message, "What can i do?", reply_markup = markup_help)
+    bot.reply_to(message, "What can i do?", reply_markup = key_markup)
+
+@bot.message_handler()
+def keyboard(message):
+    if message.text == "One":
+        bot.send_message(message.chat.id, "You tapped on the first button.")
+    elif message.text == "Two":
+        bot.send_message(message.chat.id, "You tapped on the second button.")
+    elif message.text == "Three":
+        bot.send_message(message.chat.id, "You tapped on the third button.")
 
 bot.infinity_polling()
