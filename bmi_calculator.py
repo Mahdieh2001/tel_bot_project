c1e44f9d354f5db86ee9e03a92c6939c46879b01
Mahdieh2001 <manouchehrirad2001@gmail.com>
2023-05-06 15:27:50 +0330

    add a list of weight and date

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,9 +1,10 @@
 import telebot
+import datetime
 
 bot = telebot.TeleBot("5906211167:AAFG-i9_ljPz_HR6Pwb_5uCfruU4ROmQt8M")
 
 key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
-key_markup.add("calculate BMI")
+key_markup.add("calculate BMI","weight chart")
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
@@ -36,7 +37,16 @@ def height(message):
   
     elif ( bmi >=30):
         health_condition = "suffering from Obesity"
+    now = datetime.datetime.now()
+    current_date = now.strftime("%Y:%m:%d")
+    global lst_wit_time
+    lst_wit_date = [wit, current_date]
+    bot.send_message(message.chat.id, f"your BMI is: {round(bmi, 2)}\n \nYou are {health_condition}  {lst_wit_date}")
 
-    bot.send_message(message.chat.id, f"your BMI is: {round(bmi, 2)}\n \nYou are {health_condition}")
+
+@bot.message_handler(func=lambda m: True)
+def info(message):
+    if message.text == "weight chart":
+        bot.send_message(message.chat.id, lst)
     
 bot.infinity_polling()
