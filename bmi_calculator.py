b3d05bd2c483c521414b4a9cb41cc9c7746979c4
Mahdieh2001 <manouchehrirad2001@gmail.com>
2023-05-06 14:42:46 +0330

    clean useless things

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -2,8 +2,8 @@ import telebot
 
 bot = telebot.TeleBot("5906211167:AAFG-i9_ljPz_HR6Pwb_5uCfruU4ROmQt8M")
 
-key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-key_markup.add("calculate BMI", "weight changes chart")
+key_markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+key_markup.add("calculate BMI")
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
@@ -18,13 +18,25 @@ def info(message):
 def weight(message):
     global wit
     wit = float(message.text)
-    msg = bot.reply_to(message, "Enter your height in meter")
+    msg = bot.reply_to(message, "Enter your height in centimeter")
     bot.register_next_step_handler(msg, height)
 
 def height(message):
     global hit
     hit = float(message.text)
-    bmi = wit / (hit*hit)
-    bot.send_message(message.chat.id, f"your BMI is: {round(bmi, 2)}")
+    bmi = wit*10000 / (hit*hit)
+    if (bmi < 18.5):
+        health_condition = "underweight"
+  
+    elif ( bmi >= 18.5 and bmi < 24.9):
+        health_condition = "healthy"
+  
+    elif ( bmi >= 24.9 and bmi < 30):
+        health_condition = "overweight"
+  
+    elif ( bmi >=30):
+        health_condition = "suffering from Obesity"
+
+    bot.send_message(message.chat.id, f"your BMI is: {round(bmi, 2)}\n \nYou are {health_condition}")
     
 bot.infinity_polling()
